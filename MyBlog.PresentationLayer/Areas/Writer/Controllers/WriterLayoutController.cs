6848170177af26ceed1746c7d2d00e0a92f8810d
OKAN AKSU <okanaksu_58@hotmail.com>
2024-04-23 18:45:17 +0300

    ....

---
+++ MyBlog.PresentationLayer/Areas/Writer/Controllers/WriterLayoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.PresentationLayer.Areas.Writer.Controllers
+{
+
+    [Area("Writer")]
+    public class WriterLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
