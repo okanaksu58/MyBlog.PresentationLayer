a75f43cd03b6e608c1267d22a3dae39c29f9c69f
OKAN AKSU <okanaksu_58@hotmail.com>
2024-04-23 17:10:50 +0300

    ..

---
+++ MyBlog.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyBlog.EntityLayer.Concrete;
+using MyBlog.PresentationLayer.Models;
+
+namespace MyBlog.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(RegisterViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Surname = model.Surname,
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(appUser, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
