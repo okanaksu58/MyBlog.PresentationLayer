4755e451f90a27b2418e96f61d246d1f27b3a631
OKAN AKSU <okanaksu_58@hotmail.com>
2024-04-23 16:54:28 +0300

    ..

--- MyBlog.DateAccessLayer/Migrations/20240423135316_mig5.Designer.cs
+++ MyBlog.DateAccessLayer/Migrations/20240423135316_mig5.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyBlog.DateAccessLayer.Context;
 
@@ -11,9 +12,10 @@ using MyBlog.DateAccessLayer.Context;
 namespace MyBlog.DateAccessLayer.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20240423135316_mig5")]
+    partial class mig5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
